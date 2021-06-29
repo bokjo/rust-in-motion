7b28f1211fc9bd504e7d3635dedcc5143cd2ecdf
Bojanche Stojchevski <b.stojchevski@propellor.eu>
2021-06-29 13:55:22 +0200

    feat: variables

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,12 @@ fn main() {
     let big_name = name.to_uppercase();
 
     println!("{}", big_name);
+
+    let mut x: i64 = 5;
+    x += 1;
+
+    let y: i32 = 42;
+    let z = x + y as i64;
+
+    println!("z is {}", z);
 }
